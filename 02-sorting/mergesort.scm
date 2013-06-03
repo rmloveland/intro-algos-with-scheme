c3a59fd93be95f8d9f795778a8e3ae14b16890d3
Rich Loveland <loveland.richard@gmail.com>
2013-06-03 08:06:00 -0400

    New procedure definitions. Working on bottom-up merge sort.
    Fixed some problems making the file un-`eval-buffer'-able.

--- 02-sorting/mergesort.scm
+++ 02-sorting/mergesort.scm
@@ -1,4 +1,15 @@
+;;; Sorting procedures.
+
+;; Note that we will be using a number of procedures defined in SRFI-1
+;; (http://srfi.schemers.org/srfi-1/srfi-1.html). These procedures are
+;; already available in MIT/GNU Scheme's (user) environment. In other
+;; words, you don't have to do anything, they're already baked in.
+
 (load-option 'format)
+
+;;; Recursive mergesort
+
+;; First, we need a `merge' procedure.
 
 (define (rml/merge pred l r)
   (letrec ((merge-aux
@@ -9,7 +20,7 @@
 		    ((and (not (null? left))
 			  (not (null? right)))
 		     (if (pred (car left)
-			    (car right))
+                               (car right))
 			 (merge-aux pred
                                     (cdr left)
 				    right
@@ -25,7 +36,9 @@
 		    (else #f)))))
     (merge-aux pred l r '())))
 
-(define (rml/merge-sort xs pred #!optional debug-print)
+;; Recursive merge sort (note tree recursion)
+
+(define (rml/merge-sort1 xs pred #!optional debug-print)
   (let loop ((xs xs))
 	      ; If xs is empty or has 1 element, consider it sorted
 	      ; and return it
@@ -39,10 +52,48 @@
 			(format #t "merging:~%~A~%~A~%" left right))
 		    (rml/merge pred left right)))))
 
+;; The above two procedures merged into one
+
+(define (rml/merge-sort2 xs pred #!optional debug-print)
+  (let loop ((xs xs))
+    ;; If xs is empty or has 1 element, consider it sorted
+    ;; and return it
+    (if (<= (length xs) 1)
+        xs
+        ;; Otherwise, split xs into left and right sublists and call yourself recursively
+        (let ((middle (quotient (length xs) 2)))
+          (if (not (default-object? debug-print))
+              (format #t "merging:~%~A~%~A~%" left right))
+          (let merge
+              ((pred pred) 
+               (left (loop (take xs middle)))
+               (right (loop (drop xs middle))) 
+               (result '()))
+            (cond ((and (null? left)
+                        (null? right))
+                   (reverse result))
+                  ((and (not (null? left))
+                        (not (null? right)))
+                   (if (pred (car left)
+                             (car right))
+                       (merge pred
+                              (cdr left)
+                              right
+                              (cons (car left) result))
+                       (merge pred
+                              left
+                              (cdr right)
+                              (cons (car right) result))))
+                  ((not (null? left))
+                   (merge pred (cdr left) right (cons (car left) result)))
+                  ((not (null? right))
+                   (merge pred left (cdr right) (cons (car right) result)))
+                  (else #f)))))))
+
 ;; Ported from the MIT Scheme `merge-sort!' implementation, which uses
 ;; vectors and is way fast. This uses lists and is way slow, slower
-;; even than the tree-recursive implementation above (at least until
-;; the stack blows up due to the tree recursion).
+;; even than the tree-recursive implementation above.
+
 (define (rml/merge-sort! xs pred)
   (let sort-sublist
       ((xs xs)
@@ -70,13 +121,88 @@
 ;;; Bottom-up merge sort
 
 ;; First, do just what the procedure's name implies: explode the list
-;; into lots of little one-element lists.
+;; into lots of little two-element lists. Note that the list you pass
+;; to this procedure must be already flattened.
+
 (define (explode xs)
   (let loop
       ((xs xs)
        (ys '()))
     (if (null? xs)
-	ys
-	(loop (cdr xs)
-	      (cons (list (car xs))
-		    ys)))))
+        ys
+        (loop (cdr xs)
+              (cons (list (car xs))
+                    ys)))))
+
+;; Now the bottom up merge sort!
+;; The steps are:
+;; 1. Pass through the list, merging sublists of size n.
+;; 2. Repeat for subarrays of size 2n.
+;;
+;; Example:
+;((5) (1) (2) (7) (8) (4) (3) (6))
+;((1 5) (2) (7) (8) (4) (3) (6))
+;((1 5) (2 7) (8) (4) (3) (6))
+;((1 5) (2 7) (4 8) (3) (6))
+;((1 5) (2 7) (4 8) (3 6))
+;((1 2 5 7) (4 8) (3 6))
+;((1 2 5 7) (3 4 6 8))
+;(1 2 3 4 5 6 7 8)
+
+(define (rml/merge-sort3 xs pred)
+  (let ((len (length xs))
+        (ys (explode xs)))
+    (let loop
+        ((xs xs)
+         (ys ys))
+      (if (= len (length ys))
+          ys
+          '()))))
+
+;;; Insertion sort (example of ease of implementation vs. performance
+
+(define (rml/insertion-sort1 xs pred)
+  (let loop ((xs xs)
+             (result '()))
+    (if (null? xs)
+        result
+        (loop (cdr xs)
+              (rml/merge pred
+                         (list (first xs))
+                         result)))))
+
+(define (rml/insertion-sort2 xs pred)
+  (let ((exploded (explode xs)))
+    (fold-right (lambda (x y) (rml/merge pred x y)) '() exploded)))
+
+;;; Measuring performance
+
+;; Setting up our lists to sort
+;(define unsorted (make-list-of-random-numbers 2000 2000000))
+;(define unsorted-huge (make-list-of-random-numbers 20000 2000000))
+;(define unsorted-eight '(6 3 4 8 7 2 1 5))
+
+;(with-timing-output (sort unsorted-huge <))
+;Run time:	.05
+;GC time:	.05
+;Actual time:	.103
+
+;(with-timing-output (rml/insertion-sort1 unsorted <))
+;Run time:	13.
+;GC time:	.33
+;Actual time:	13.485
+
+;(with-timing-output (rml/insertion-sort2 unsorted <))
+;Run time:	12.95
+;GC time:	.34
+;Actual time:	13.444
+
+;(with-timing-output (rml/merge-sort1 unsorted-huge <))
+;Run time:	1.97
+;GC time:	.05
+;Actual time:	2.149
+
+;(with-timing-output (rml/merge-sort2 unsorted-huge <))
+;Run time:	2.24
+;GC time:	.05
+;Actual time:	2.35
