7f360ae772b7ce6c9a558656111efa249ae3ef5e
Rich Loveland <loveland.richard@gmail.com>
2013-06-12 08:33:12 -0400

    Add a `main.scm' file in toplevel dir to load all the book's code.

---
+++ main.scm
@@ -0,0 +1,21 @@
+;;; main.scm --- Load all code needed to work through the book.
+
+(load-option 'format)
+(load-option 'regular-expression)
+
+(define book-dir 
+  (string-append 
+   (get-environment-variable "HOME")
+   "/Desktop/current/intro-algos-with-scheme/"))
+
+(define book-files
+  (map (lambda (fname)
+         (string-append book-dir fname))
+       '("/00-introduction/test-lib.scm"
+         "/01-searching/binary-search.scm"
+         "/02-sorting/mergesort.scm"
+         "/03-trees/binary-tree.scm")))
+
+(for-each (lambda (book-file)
+            (load book-file))
+          book-files)
