6753280b51f1deac2f0b0994c24efdc8fc2d3c57
Rich Loveland <loveland.richard@gmail.com>
2013-07-05 14:35:19 -0400

    First rough working version of bottom-up merge sort. More testing required.

--- 02-sorting/mergesort.scm
+++ 02-sorting/mergesort.scm
@@ -165,17 +165,23 @@
 
 (define (rml/merge-sort3 xs pred)
   (let ((exploded (explode xs)))
-    (let loop
-        ((exploded exploded)
-         (result '()))
-      (if (null? exploded)
-          result
-          (loop (cddr exploded)
-                (cons
-                 (rml/merge pred
-                            (first exploded)
-                            (second exploded))
-                 result))))))
+    (let loop ((exploded exploded)
+               (result '()))
+      (cond ((and (null? exploded)
+                  (= 1 (length result)))
+             (car result))
+            ((null? exploded)
+             (loop result
+                   exploded))
+            ((null? (cdr exploded))
+             (loop (cdr exploded)
+                   (cons (car exploded) result)))
+            (else
+             (loop (cddr exploded)
+                   (cons (rml/merge <
+                                    (first exploded)
+                                    (second exploded))
+                         result)))))))
 
 ;;; Insertion sort (example of ease of implementation vs. performance)
 
