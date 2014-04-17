4725002ff5db39e934bb8b275d3989634e6b5c50
Rich Loveland <r@rmloveland.com>
2014-04-17 07:23:57 -0400

    Update procedure doc.

--- 00-introduction/test-lib.scm
+++ 00-introduction/test-lib.scm
@@ -1,7 +1,7 @@
 ;;; test-lib.scm --- A simple testing library.
 
 (define (assert pred expected got #!optional noise)
-  ;; Proc Object Object Any -> IO
+  ;; Proc Value Value . Value -> IO!
   (if (pred expected got)
       (format #t "~A is ~12@A\t... ok~%" expected got)
       (format #t "~A is not ~8@A\t... FAIL~%" expected got)))
