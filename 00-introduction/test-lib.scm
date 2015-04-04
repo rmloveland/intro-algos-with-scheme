5012ab895d9bda7e971d272f90f483c4c2d1a5b5
Richard M. Loveland <r@rmloveland.com>
2015-04-04 14:14:14 -0400

    Convert binary search code to S48 0.53.

--- 00-introduction/test-lib.scm
+++ 00-introduction/test-lib.scm
@@ -1,7 +1,13 @@
 ;;; test-lib.scm --- A simple testing library.
 
-(define (assert pred expected got #!optional noise)
-  ;; Proc Value Value . Value -> IO!
+(define (assert pred expected got msg . noise)
+  ;; Proc Value Value String . Value -> IO!
+  (format #t "Testing condition '~A'~% " msg)
   (if (pred expected got)
-      (format #t "~A is ~12@A\t... ok~%" expected got)
-      (format #t "~A is not ~8@A\t... FAIL~%" expected got)))
+      (format #t "~A is ~A... ok~%~%" expected got)
+      (format #t "~A is not ~A... FAIL~%~%" expected got)))
+
+;; ++ Should this library also have a summarizing procedure that
+;; prints the results of the entire test run?
+
+;;; eof
--- 01-searching/binary-search.scm
+++ 01-searching/binary-search.scm
@@ -1,4 +1,10 @@
-;;; A binary search implementation in MIT/GNU Scheme.
+;;;; binary-search.scm
+
+(define %random (make-random 314159))	; ,open random
+
+(define (random n)
+  ;; Int -> Int
+  (modulo (%random) n))
 
 (define (make-list-of-random-numbers list-length max)
   ;; Int Int -> List
@@ -7,48 +13,30 @@
             (lambda (list-length max result)
               (let loop ((n list-length) (result '()))
                 (if (= n 0)
-                    result 
+                    result
                     (loop (- n 1) (cons (random max) result)))))))
     (maker list-length max '())))
 
-(define (re-matches? rx line #!optional display-matches)
-  ;; Rexp String . Boolean -> Boolean
-  "Attempt to match RX against LINE. Print the match if DISPLAY-MATCHES is set."
-  (let ((match (re-string-match rx line)))
-    (if match
-	(if (not (default-object? display-matches))
-	    (begin (format #t "|~A|~%" (re-match-extract line match 0))
-		   #t)
-	    #t)
-	#f)))
-
-(define (make-list-of-words-matching re file)
-  ;; Rexp String -> List
-  "Given a regular expression RE, loop over FILE, gathering matches."
-  (call-with-input-file file
-    (lambda (port)
-      (let loop ((source (read-line port)) (sink '()))
-        (if (eof-object? source)
-            sink
-            (loop (read-line port) (if (re-matches? re source)
-                             (cons source sink)
-                             sink)))))))
-
-(define (->int n)
-  ;; Number -> Int
+(define (number->integer n)
+  ;; Num -> Int
   "Given a number N, return its integer representation.
 N can be an integer or flonum (yes, it's quick and dirty)."
-  (flo:floor->exact (exact->inexact n)))
+  (cond ((floatnum? n)			; ,open floatnums
+	 (floor (inexact->exact n)))
+	((integer? n) n)
+	((exact? n)
+	 (floor (exact->inexact n)))
+	(else #f)))			; BOOM!!!
 
 (define (split-difference low high)
   ;; Int Int -> Int
   "Given two numbers, return their rough average."
   (if (= (- high low) 1)
       1
-      (->int (/ (- high low) 2))))
+      (number->integer (/ (- high low) 2))))
 
-(define (binary-search word xs #!optional debug-print)
-  ;; String List -> Int
+(define (binary-search word xs debug-print?)
+  ;; Str List -> Int
   "Do binary search of list XS for WORD. Return the index found, or #f."
   (if (null? xs)
       #f
@@ -63,41 +51,44 @@ N can be an integer or flonum (yes, it's quick and dirty)."
                 low
                 #f))
 	   ((string<? word-at-try word)
-	    (if (not (default-object? debug-print))
-		(begin (format #f "(string<? ~A ~A) -> #t~%try: ~A high: ~A low: ~A ~2%"
+	    (if debug-print?
+		(begin (format #f "(string<? ~A ~A) -> #t~%try: ~A high: ~A low: ~A ~%" ; ,open formats
 			       word-at-try word try high low)
 		       (loop (+ 1 try) high)) ; raise the bottom of the window
 		(loop (+ 1 try) high)))
 	   ((string>? word-at-try word)
-	    (if (not (default-object? debug-print))
-		(begin (format #f "(string>? ~A ~A) -> #t~%try: ~A high: ~A low: ~A ~2%"
+	    (if debug-print?
+		(begin (format #f "(string>? ~A ~A) -> #t~%try: ~A high: ~A low: ~A ~%"
 			       word-at-try word try high low)
 		       (loop low (+ 1 try))) ; lower the top of the window
 		(loop low (+ 1 try))))
 	   (else #f))))))
 
+(define (words-file)
+  ;; -> Str
+  "/home/rml/Code/intro-algos-with-scheme/01-searching/words.scm")
+
+(define *words* (load (words-file)))	; IO!
+
 (define (run-binary-search-tests)
-  ;; -> IO
-  "Run our binary search tests using known words from the 'words' file.
-This file should be in the current working directory."
-  (with-working-directory-pathname (pwd)
-    (lambda ()
-      (if (file-exists? "words")
-          (begin
-            (format #t "file 'words' exists, making a list...~%")
-            (let* ((unsorted (make-list-of-words-matching "acc" "words"))
-                   (sorted (sort unsorted string<?)))
-              (format #t "doing binary searches...~%")
-              (assert equal? #f (binary-search "test" '())) ; empty list
-              (assert equal? #f (binary-search "aardvark" sorted)) ; element absent and too small
-              (assert equal? #f (binary-search "zebra" sorted)) ; element absent and too large
-              (assert = 0 (binary-search "accusive" '("accusive"))) ; list of length one
-              (assert = 0 (binary-search "acca" sorted)) ; first element of list
-              (assert = 1 (binary-search "aardvark" '("aardvark" "aardvark" "babylon"))) ; multiple copies of word in list
-              (assert = 1 (binary-search "barbaric" '("accusive" "barbaric"))) ; list of length two
-              (assert = 98 (binary-search "acclamator" sorted))
-	      (assert = 127 (binary-search "aardvark" (map (lambda (x) "aardvark") sorted))) ; list is all one value
-              (assert = 143 (binary-search "accomplice" sorted))
-              (assert = 254 (binary-search "accustomedly" sorted))
-              (assert = 255 (binary-search "accustomedness" sorted)))) ; last element of list
-          (error "File `words' not found in this directory")))))
+  ;; -> IO!
+  "Run our binary search tests using known words from the 'words' file."
+  (begin
+    (let* ((unsorted (load (words-file)))
+	   (sorted (sort-list unsorted string<?))) ; ,open sort
+      (format #t "Running binary search tests...~%")
+      (assert equal? #f (binary-search "test" '() #f) "element absent: list is empty")
+      (assert equal? #f (binary-search "aardvark" sorted #f) "element absent: too small")
+      (assert equal? #f (binary-search "zebra" sorted #f) "element absent: too large")
+      (assert = 0 (binary-search "accusive" '("accusive") #f) "element present: list of length one")
+      (assert = 0 (binary-search "acca" sorted #f) "element present: first item in list")
+      (assert = 255 (binary-search "accustomedness" sorted #f) "element present: last item in list")
+      (assert = 1 (binary-search "aardvark" '("aardvark" "aardvark" "babylon") #f) "element present: multiple copies of word in list")
+      (assert = 1 (binary-search "barbaric" '("accusive" "barbaric") #f) "element present: list of length two")
+      (assert = 98
+	      (binary-search "acclamator" sorted #f) "element present: general")
+      (assert = 127 (binary-search "aardvark" (map (lambda (x) "aardvark") sorted) #f) "element present: list is all one value")
+      (assert = 143 (binary-search "accomplice" sorted #f) "element present: general")
+      (assert = 254 (binary-search "accustomedly" sorted #f) "element present: general"))))
+
+;;; eof
