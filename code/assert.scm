b9e96ea7fd43d12d3566ec042790545825c56fd4
Rich Loveland <r@rmloveland.com>
2016-05-23 06:21:06 -0700

    Remove some newlines from test output

--- code/assert.scm
+++ code/assert.scm
@@ -2,10 +2,10 @@
 
 (define (assert pred expected got msg . noise)
   ;; Proc Value Value String . Value -> IO!
-  (format #t "Testing condition '~A'~%~%" msg)
+  (format #t "Testing condition '~A'~%" msg)
   (if (pred expected got)
-      (format #t "~A is ~A... ok~%~%" expected got)
-      (format #t "~A is not ~A... FAIL~%~%" expected got)))
+      (format #t "~A is ~A ... ok~%" expected got)
+      (format #t "~A is not ~A ... FAIL~%" expected got)))
 
 ;; ++ Should this library also have a summarizing procedure that
 ;; prints the results of the entire test run?
