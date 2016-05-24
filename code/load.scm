2ec63c91ead2b7bdd177b0dc35abbc7e4f3e7a5d
Rich Loveland <r@rmloveland.com>
2016-05-24 06:47:49 -0700

    Add comment listing needed packages

--- code/load.scm
+++ code/load.scm
@@ -1,3 +1,5 @@
+;; ,open pp random floatnums formats sort srfi-8 srfi-9 srfi-13
+
 (define (load-book-files)
   (let ((the-directory
          (expand-file-name "~/Documents/personal/intro-algos-with-scheme/code/")))
