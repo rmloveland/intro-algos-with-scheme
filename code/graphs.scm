a874a1c4c68b515285c33a0e5f4e0a90727a8367
Rich Loveland <r@rmloveland.com>
2017-05-23 09:08:33 -0400

    Add unweighted, undirected graph implementation

--- code/graphs.scm
+++ code/graphs.scm
@@ -1,158 +1,92 @@
-;; dfs.scm --- depth first searching of graphs
+;;; graphs.scm
 
-;; adjacency list, sedgewick p. 421
+;; 1. Unweighted, undirected graphs
 
-;; graphs can be represented with matrices or alists
+;; This code is adapted from my "Simple network search in Scheme" blog
+;; post. The implementation is a port to Scheme of the Common Lisp
+;; code in Chapter 19 of Winston and Horn's book *Lisp*.
 
-(define *the-graph* '((LEN . 13)
-		      (A  (F C B G))
-		      (B  (A))
-		      (C (A))
-		      (D  (F E))
-		      (E  (G F D))
-		      (F  (A E D))
-		      (G  (E A))
-		      (H  (I))
-		      (I  (H))
-		      (J  (K L M))
-		      (K  (J))
-		      (L  (J M))
-		      (M  (J L))))
+;; In this adjacency list representation, each `(CAR . CDR)` pair
+;; represents a
+;; `(NODE . NEIGHBORS)` relationship.
 
-;; dfs is a natural way to visit every node and check every edge in
-;; the graph systematically.
+;; For more information about the properties of the adjacency list
+;; representation of a graph, see Sedgewick, p. 421 (2nd ed.).
 
-'(define (dfs nodes start-id visited)
-   (let loop ((nodes nodes)
-              (id start-id)
-              (visited visited)
-              (sum 0))
-     (if (null? nodes)
-         sum
-         (let ((neighbour (car nodes)))
-           (begin (vector-set! visited id #t)
-                  (format #t "At ~A, visiting ~A next~%"
-                          id (route-dest neighbour))
-                  (loop (cdr nodes)
-                        (route-dest neighbour)
-                        visited
-                        (+ sum (route-cost neighbour))))))))
+;; We can either define the graph statically...
 
-'(define (depth-first-search the-graph)	;Ignore.
-  (let* ((len (cdr (assoc 'LEN the-graph)))
-	 (the-graph* (cdr the-graph))
-	 (val (make-vector len #f)))
-    (let loop ((k 0)))))
+(define *graph* '((f e)
+                  (e b d f)
+                  (d s a e)
+                  (c b)
+                  (b a c e)
+                  (a s b d)
+                  (s a d)))
 
-
+;; ... or build it up stepwise.
 
-;;; This code came from longest-path.scm.  Work will be needed to
-;;; merge it into graphs.scm.
+(define (make-graph)
+  '())
 
-;; ,open srfi-8 srfi-9 srfi-13 pp
+(define *neighbors* (make-graph))
 
-(define *graph-file*
-  (expand-file-name
-   "~/Documents/personal/intro-algos-with-scheme/data/smallgraph.dat"))
+(define (add-neighbor! k v graph)
+  (let ((new-neighbor (cons k v)))
+    (push! new-neighbor `,graph)))
 
-(define-record-type :route		;SRFI-9
-  (make-route dest cost)
-  route?
-  (dest route-dest)
-  (cost route-cost))
+(define-syntax add-neighbor!
+  (syntax-rules ()
+    ((_ k v ?graph)
+     (let ((new-neighbor (cons k v)))
+       (push! new-neighbor ?graph)))))
 
-(define-record-type :node
-  (make-node id neighbour distance)
-  node?
-  (id node-id)
-  (neighbour node-neighbour)
-  (distance node-distance))
+(add-neighbor! 's '(a d) *neighbors*)
+(add-neighbor! 'a '(s b d) *neighbors*)
+(add-neighbor! 'b '(a c e) *neighbors*)
+(add-neighbor! 'c '(b) *neighbors*)
+(add-neighbor! 'd '(s a e) *neighbors*)
+(add-neighbor! 'e '(b d f) *neighbors*)
+(add-neighbor! 'f '(e) *neighbors*)
 
-(define (parse-line line)
-  ;; Str -> Node
-  (if (eof-object? line) '()
-      (let* ((pieces (map
-		      (lambda (s) (string->number s))
-		      (string-tokenize line)))
-             (id (first pieces))
-	     (neighbour (second pieces))
-	     (distance (third pieces)))
-	(make-node id neighbour distance))))
+(define (get-neighbor k)
+  ;; List -> Any
+  (let ((val (assoc k *neighbors*)))
+    (if val (cdr val) '())))
 
-(define (read-places)
-  ;; -> List of Nodes, Int
-  (with-input-from-file *graph-file*
-    (lambda ()
-      (let ((num-lines (string->number (read-line))))
-	(let loop ((line #f)
-		   (retval '()))
-	  (if (null? line)
-	      (values retval num-lines)
-	      (let ((line (parse-line (read-line))))
-		(loop line (cons (parse-line (read-line)) (cons line retval))))))))))
+;; Every time we visit a node, we want to extend our partial paths
+;; list with that node.  (A.K.A. our "seen" list.)
 
-(define (parse-places)
-  (receive (place-data num-nodes)	;srfi-8
-      (read-places)
-    (let ((nodes (make-vector num-nodes #f)))
-      (let loop ((place-data place-data))
-	(cond ((null? place-data) nodes)
-	      ((node? (car place-data))
-	       (let* ((node (car place-data))
-		      (id (node-id node))
-		      (neighbour (node-neighbour node))
-		      (distance (node-distance node))
-		      (route (make-route neighbour distance)))
-		 (vector-set! nodes id route)
-		 (loop (cdr place-data))))
-	      (else (loop (cdr place-data))))))))
+(define (extend-path path)
+  (display (reverse path))
+  (newline)
+  (map (lambda (new-node)
+         (cons new-node path))
+       ;; The `FILTER` is necessary here so we don't build circular
+       ;; paths that keep us from ever finding the node we want.
+       (filter (lambda (neighbor)
+                 (not (member neighbor path)))
+               (get-neighbor (first path)))))
 
-(define (get-longest-path nodes id visited)
-  ;; List Int Vec -> Int
-  (let ((node-id id))
-    (vector-set! visited node-id #t)
-    (let ((max 0))
-      (map (lambda (neighbour)
-             (if (not (vector-ref visited (route-dest neighbour)))
-                 (let ((distance (+ (route-cost neighbour)
-                                    (get-longest-path nodes
-                                                      (route-dest neighbour)
-                                                      visited))))
-                   (if (> distance max)
-                       (set! max distance)))))
-           nodes)
-      (vector-set! visited node-id #f)
-      max)))
+;; If `BREADTH-FIRST?` is true, perform a breadth-first search.
+;; Otherwise, use depth-first search.
 
-;; works for DFS (depth-first search), but not longest path; gets 8929
-;; but correct value is 8981
+(define (find-path-between start finish network breadth-first?)
+  (let ((queue (list (list start))))
+    (let loop ((start start)
+               (finish finish)
+               (network network)
+               (queue queue))
+      (cond ((null? queue) '())                    ;Queue empty?
+            ((equal? finish (first (first queue))) ;Finish found?
+             (reverse (first queue)))              ;Return path.
+            (else
+             (if breadth-first?
+                 (loop start finish network
+                       (append
+                        (rest queue)
+                        (extend-path (first queue))))
+                 (loop start finish network
+                       (append
+                        (extend-path (first queue))
+                        (rest queue)))))))))
 
-(define (dfs nodes start-id visited)
-  (let loop ((nodes nodes)
-             (id start-id)
-             (visited visited)
-             (sum 0))
-    (if (null? nodes)
-        sum
-        (let ((neighbour (car nodes)))
-          (begin (vector-set! visited id #t)
-                 (format #t "At ~A, visiting ~A next~%"
-                         id (route-dest neighbour))
-                 (loop (cdr nodes)
-                       (route-dest neighbour)
-                       visited
-                       (+ sum (route-cost neighbour))))))))
-
-(define (run-it proc)
-  (let* ((nodes (parse-places))
-         (nodes* (vector->list nodes))
-         (visited (make-vector (vector-length nodes) #f))
-         (the-answer (proc nodes* 0 visited)))
-    (display nodes*) (newline)
-    (display the-answer) (newline)))
-
-(define (show-data-structures)          ;For debugging.
-  (let* ((nodes (parse-places))
-	 (nodes* (vector->list nodes))
-	 (visited (make-vector (vector-length nodes) #f)))
-    (for-each (lambda (x) (map (lambda (y) (p y)) x)) (list nodes*))))
