9c0e3f1a159739df448c3bce53af786bcd885437
Richard Loveland <r@rmloveland.com>
2017-06-24 00:06:54 -0400

    Tweak test output formatting

--- code/assert.scm
+++ code/assert.scm
@@ -4,8 +4,8 @@
   ;; Proc Value Value String . Value -> IO!
   (format "Testing condition '~a'~%" msg)
   (if (pred expected got)
-      (format "~a is ~a ... ~a~%~%" expected got #t)
-      (format "~a is not ~a ... ~a~%" expected got #f)))
+      (format "  ~a~%  is~%  ~a~%  ~a~%~%" expected got #t)
+      (format "  ~a~%  is not~%  ~a~%  ~a~%" expected got #f)))
 
 ;; ++ Should this library also have a summarizing procedure that
 ;; prints the results of the entire test run?
