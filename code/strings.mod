b30269e74747ecba6db597308aeab70bb2d9204e
Rich Loveland <r@rmloveland.com>
2018-01-13 11:34:56 -0500

    Add beginnings of a string utility library

---
+++ code/strings.mod
@@ -0,0 +1,12 @@
+;;; -*- Mode: Scheme -*-
+
+(define-module strings
+  (exports
+   string-split
+   pregexp-replace
+   pregexp-replace*
+   pregexp-quote
+   *pregexp-comment-char*)
+  (requires pregexp))
+
+;; eof
---
+++ code/strings.scm
@@ -0,0 +1,23 @@
+;;; -*- Mode: Scheme -*-
+
+;; strings -- helpers for string processing
+
+(define string-split pregexp-split)
+
+(define (string-trim s . which)
+  ;; 'LEFT 'RIGHT, default to both
+  (let ((rx-left "^[ \t]+")
+        (rx-right "[ \t]+$"))
+    (cond ((null? which)
+           (let* ((s1 (string-trim-left s))
+                  (s2 (string-trim-right s1)))
+             s2))
+          ((equal? which '(LEFT)) (string-trim-left s))
+          ((equal? which '(RIGHT)) (string-trim-right s))
+          (else (error "STRING-TRIM - bad arguments: " s which)))))
+
+(define (string-trim-right s)
+  (pregexp-replace "[ \t]+$" s ""))
+
+(define (string-trim-left s)
+  (pregexp-replace "^[ \t]+" s ""))
