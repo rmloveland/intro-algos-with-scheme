bfc41bcb4b91ffe5a983e00f38ceb2204dfe59ee
Rich Loveland <r@rmloveland.com>
2018-01-13 12:41:24 -0500

    Work around a cross-implementation ambiguity

--- code/io.scm
+++ code/io.scm
@@ -2,9 +2,19 @@
 
 ;; io.scm -- Utilities for file input/output
 
-;; XXX This implementation of `READ-LINE` has a bug.  We know this
-;; because `MAP-LINES` (below) works just fine with Larceny's
-;; implementation of `READ-LINE`.
+;; The implementation of `READ-LINE` given here may (?) have a bug (or
+;; it may just not be R7RS).  Its caller `MAP-LINES` (below) works
+;; just fine with Larceny 1.3's default implementation of `READ-LINE`,
+;; but inserts an extra "" in between each element if the
+;; implementation below is used.  However, everything here works as
+;; expected on Chez 9.5 and Scheme48 1.9.
+
+;; For more information about the Larceny behavior and why it may be
+;; correct, see the following:
+;; 
+;; - https://github.com/larcenists/larceny/issues/748
+;; 
+;; - https://github.com/larcenists/larceny/commit/6f0ce4455c
 
 (define (read-line port)
   ;; Port -> String
@@ -20,6 +30,9 @@
                    (peek-char port)))))))
 
 (define (map-lines file)
+  (filter (lambda (x) (not (string=? x ""))) (%map-lines file)))
+
+(define (%map-lines file)               ; Internal
   (call-with-input-file file
     (lambda (port)
       (let loop ((line (read-line port))
